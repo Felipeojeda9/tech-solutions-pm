dd52792d51f61b07fcdc98263ae1b4005027bea9
Felipeojeda9 <166962367+Felipeojeda9@users.noreply.github.com>
2025-08-17 03:30:40 -0400

    Add files via upload

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,16 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+class DatabaseSeeder extends Seeder{
+    public function run(): void 
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+            'email' => 'test@ejemplo.com',
+            'password' => bcrypt('Secret123!'),
+            ]);
     }
 }
